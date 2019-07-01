31ba98b47823d476c536017fc851c949992767ee
Ayush Palak <ayushpalakcs@gmail.com>
2019-07-01 06:59:54 +0530

    added new feattures

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import {Person,Dog} from './Person/Person.js'
 
 
 class App extends Component {
+  state = {name:"Golden Retriever"};
+  changeNameState = (newName) =>{
+    this.setState({
+      name:newName
+    });
+    
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      name:event.target.value
+    });
+  }
+
+  // labrador, great dane
+  
   render() {
     return (
       <div>
         <Person name="ayush" food="biryani"> this is a Person tag. </Person>
-        <Dog value='German Shepherd'></Dog>
+        {/* passing function as a reference using bind */}
+        <Dog value={this.state.name} click={this.changeNameState.bind(this,'pug')}></Dog> 
+         {/* anonymous function is returning this.changeNameState('labrador') */}
+         {/* passing function as a reference using reference to function */}
+         {/* not very effiient */}
+        <Dog value='great dane' click={ () => this.changeNameState('labrador') }></Dog>
+        <Dog value={this.state.name} onChange={this.onChangeHandler} ></Dog>
+        
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component,useState } from 'react';
 import '../App.css'
 import { Button} from 'react-bootstrap';
+import './Person.css'
 
 class Person extends Component{
     
@@ -24,7 +25,7 @@ class Person extends Component{
     render(props) {
         if (this.state.flag === '0'){
             return (
-                <div className="person"> 
+                <div className="Person"> 
                     <Button className="click_button" onClick={this.click_Event_Handler}> Switch Name </Button>
                     <p> I am {this.state.person[0].name}. I like {this.state.person[0].food}. </p>
                     <p> I am {this.state.person[1].name}. I like {this.state.person[1].food}. </p>
@@ -32,7 +33,7 @@ class Person extends Component{
             );
         }else{
             return (
-                <div className="person"> 
+                <div className="Person"> 
                     <Button className="click_button" onClick={this.click_Event_Handler}> Switch Name </Button>
                     <p> I am Ayush. </p>
                 </div>
@@ -44,23 +45,54 @@ class Person extends Component{
 
 const Dog = (props) => {
     
-    const [currentState,updatedState] = useState({weight:'20kg'}
+    const [currentState,updatedState] = useState({weight:'20kg',name:'default', flag:false}
     
     );
     const button_click_handler = () =>{
         updatedState(
-            {weight:'25kg'}
+            {weight:'25kg',name:'default', flag:false}
+        );
+    }
+
+    // using style this way helps to scope the styling to component and we donot have to worry defining id for everything.
+    const style = {
+        "background-color": "grey",
+        "border": "1px solid blue",
+        'padding' : '8px',
+        'font' : 'inherit',
+        'cursor':'pointer'
+    };
+    // another way to write styles within component by using camel Casing.
+    const para_style = {
+        backgroundColor : 'yellow'
+    };
+    
+    const buttonClickHandler = () => {
+        var flagvalue = currentState.flag; 
+        
+        updatedState(
+            {weight:'25kg',name:'default',flag:!flagvalue}
         );
     }
     
     return (
-        <div>
-            <button onClick={button_click_handler}> change weight </button>
-            <p>
-                This is a {props.value}. Weight is {currentState.weight} 
+        <div className='Dog'>
+            <button style={style} onClick={button_click_handler}> change weight </button>
+            <p onClick={props.click} style={para_style}>
+                This is a {props.value}. Weight is {currentState.weight}
+                {currentState.name} is another dog. 
             </p> 
+            <div>
+            <input type="text" onChange={props.onChange}  />  {/*this is how input tag should be closed.*/}
+            </div>
+            <button style={style} onClick={buttonClickHandler} > toggle </button>
+            { currentState.flag ?
+            <div>
+                <p style={para_style}> This para will come and go </p>
+            </div> : null
+            }
         </div>
     );
 };
 
-export  {Person, Dog};
+export  {Person, Dog};  // this is how multiple components are exported. use default when there is only one component.
